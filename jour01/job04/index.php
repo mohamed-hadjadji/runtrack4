4a6048f6cd0ddf329c68d81a20cee2e5af534913
mohamed-hadjadji <55872437+mohamed-hadjadji@users.noreply.github.com>
2020-05-04 15:06:32 +0200

    job04 marche bien

---
+++ jour01/job04/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel='stylesheet' href='style.css'>
+	<title></title>
+</head>
+<body>
+	<header>
+
+		<nav role="navigation">
+		    <div id="menuToggle">
+		   
+			    <input type="checkbox" />
+			    		   
+			    <span></span>
+			    <span></span>
+			    <span></span>
+			    		   
+			    <ul id="menu">
+			        <a href="#"><li>Accueil</li></a>
+			        <a href="#"><li>Présentation</li></a>
+			        <a href="#"><li>Contact</li></a>
+			    </ul>
+		    </div>
+		</nav>
+    </header>
+    <main>
+	
+		<section class="section">
+			
+		</section>
+		<section class="section">
+			
+		</section>
+		<section class="section">
+			
+		</section>
+		<section class="section">
+			
+		</section>
+    </main>
+
+</body>
+</html>
