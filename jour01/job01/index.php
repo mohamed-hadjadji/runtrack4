8ba6cb5d362cddc84c3d5827591ec4a6c7c9a1aa
mohamed-hadjadji <55872437+mohamed-hadjadji@users.noreply.github.com>
2020-05-04 10:40:21 +0200

    job01 ok

---
+++ jour01/job01/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<link rel='stylesheet' href='style1.css'>
+	<link rel='stylesheet' href='style2.css'>
+	<link rel='stylesheet' href='style3.css'>
+	<link rel='stylesheet' href='style4.css'>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
