435284bc3747e69a3b4c773ad1ea5ef9fe6fa9ac
mohamed-hadjadji <55872437+mohamed-hadjadji@users.noreply.github.com>
2020-05-04 12:00:45 +0200

    job03 ok

---
+++ jour01/job03/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel='stylesheet' href='style.css'>
+	<title></title>
+</head>
+<body>
+	<section class="section">
+		
+	</section>
+	<section class="section">
+		
+	</section>
+	<section class="section">
+		
+	</section>
+	<section class="section">
+		
+	</section>
+
+</body>
+</html>
