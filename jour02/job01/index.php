a62f7d11ccc6e539960f35e8cba143c00aee9d44
mohamed-hadjadji <55872437+mohamed-hadjadji@users.noreply.github.com>
2020-05-05 11:05:46 +0200

    job01 ok

---
+++ jour02/job01/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<header>
+		<nav>
+		    <div class="">
+		    
+		      <ul id="" class="">
+		        <li><a href="index.php">Accueil</a></li>
+		        <li><a href="index.php">Inscription</a></li>
+		        <li><a href="index.php">Connexion</a></li>
+		        <li><a href="index.php">Rechercher</a></li>
+		      </ul>
+		    </div>
+		</nav>
+	</header>
+	<section>
+		<form>
+			<label>Civilité: </label>
+			<input type="radio" name="monsieur" value="M" checked>
+			<label for="M">M.</label>
+			<input type="radio" name="madame" value="Mme">
+			<label for="Mme">Mme.</label>
+			<label>Prénom: </label>
+			<input type="text" name="prenom" value="Votre prénom">
+			<label>Nom: </label>
+			<input type="text" name="nom" value="Votre nom">
+			<label>Adresse: </label>
+			<input type="text" name="prenom" value="Votre adresse">
+			<label>Email: </label>
+			<input type="text" name="email" value="Votre email">
+			<label>Password: </label>
+			<input type="text" name="mdp" value="Password">
+			<label>Confirmer Password: </label>
+			<input type="text" name="mdp2" value="Password">
+			<label>Passions: </label>
+			<input type="checkbox" id="" name="info">
+            <label for="info">Informatique</label>
+            <input type="checkbox" id="" name="voy">
+            <label for="voy">Voyages</label>
+            <input type="checkbox" id="" name="sport">
+            <label for="sport">Sport</label>
+            <input type="checkbox" id="" name="lect">
+            <label for="lect">Lecture</label>
+            <input type="submit" name="valide" value="Valider">
+		</form>
+	</section>
+
+	<footer>
+		<div class="">
+		
+		      <ul id="" class="">
+		        <li><a href="index.php">Accueil</a></li>
+		        <li><a href="index.php">Inscription</a></li>
+		        <li><a href="index.php">Connexion</a></li>
+		        <li><a href="index.php">Rechercher</a></li>
+		      </ul>
+		 </div>
+	</footer>
+
+    
+
+</body>
+</html>
