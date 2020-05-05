cb185fb30cc64394ebf95c1249fc0d20d791fbe5
mohamed-hadjadji <55872437+mohamed-hadjadji@users.noreply.github.com>
2020-05-05 16:38:47 +0200

    job05 ok

---
+++ jour02/job05/index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<title></title>
+</head>
+<body>
+	<header>
+		<nav>
+		    <div class="nav-wrapper blue lighten-3">
+		      <a href="#" class="brand-logo"><img height="70" src="https://www.makrea.com/img_pngs/SPORT-63-1.png"></a>
+		      <ul id="nav-mobile" class="right hide-on-med-and-down">
+		        <li><a href="">Accueil</a></li>
+		        <li><a href="">Connexion</a></li>
+		        <li><a href="">Inscription</a></li>
+		      </ul>
+		    </div>
+		</nav>
+	</header>
+	<br/><br/><br/>
+	<section>
+		<div class="row">
+		    <div class="col s12 m4">
+		      <div class="card">
+		        <div class="card-image">
+		          <img height="500" src="https://www.droitdunet.fr/wp-content/uploads/2016/06/f-22-raptor.jpg">
+		          <span class="card-title">F-22 Raptor</span>
+		        </div>
+		        <div class="card-content">
+		        
+		        </div>
+		        <div class="card-action">
+		          <a href="https://fr.wikipedia.org/wiki/Lockheed_Martin_F-22_Raptor">Voir plus</a>
+		        </div>
+		      </div>
+		    </div>
+		    <div class="col s12 m4">
+		      <div class="card">
+		        <div class="card-image">
+		          <img height="500" src="https://ams3.digitaloceanspaces.com/rewyer-storage-001/media/9235e9b306a472e733e72b8285236471eec2dcc339023d066b8f58168042bf5b">
+		          <span class="card-title">SU-57</span>
+		        </div>
+		        <div class="card-content">
+		        
+		        </div>
+		        <div class="card-action">
+		          <a href="https://fr.wikipedia.org/wiki/Soukho%C3%AF_Su-57">Voir plus</a>
+		        </div>
+		      </div>
+		    </div>
+		    <div class="col s12 m4">
+		      <div class="card">
+		        <div class="card-image">
+		          <img height="450" src="https://www.usinenouvelle.com/mediatheque/4/2/2/000140224_image_896x598/f35-lockheed-martin.jpg">
+		          <span class="card-title">F-35</span>
+		        </div>
+		        <div class="card-content">
+		        
+		        </div>
+		        <div class="card-action">
+		          <a href="https://fr.wikipedia.org/wiki/Lockheed_Martin_F-35_Lightning_II">Voir plus</a>
+		        </div>
+		      </div>
+		    </div>
+		</div>
+		<div class="row">		    
+		    <div class="col s12 m4">
+		      <div class="card">
+		        <div class="card-image">
+		          <img src="https://ams3.digitaloceanspaces.com/rewyer-storage-001/media/a828c168c64944f9b4dc0f5c315ef51a3ef2d8984dffd2d406eb329e61be2922.jpg">
+		          <span class="card-title">Rafale</span>
+		        </div>
+		        <div class="card-content">
+		          
+		        </div>
+		        <div class="card-action">
+		          <a href="https://fr.wikipedia.org/wiki/Dassault_Rafale">Voir plus</a>
+		        </div>
+		      </div>
+		    </div>
+		
+
+		    <div class="col s12 m4">
+		      <div class="card">
+		        <div class="card-image">
+		          <img height="450" src="https://static.timesofisrael.com/fr/uploads/2018/05/MAKS2015part1-10_cropped.jpg">
+		          <span class="card-title">SU-34</span>
+		        </div>
+		        <div class="card-content">
+		          
+		        </div>
+		        <div class="card-action">
+		          <a href="https://fr.wikipedia.org/wiki/Soukho%C3%AF_Su-34">Voir plus</a>
+		        </div>
+		      </div>
+		    </div>
+		    <div class="col s12 m4">
+		      <div class="card">
+		        <div class="card-image">
+		          <img src="https://i.ytimg.com/vi/bawBxxKrWnw/maxresdefault.jpg">
+		          <span class="card-title">Future</span>
+		        </div>
+		        <div class="card-content">
+		          
+		        </div>
+		        <div class="card-action">
+		          <a href="https://fr.wikipedia.org/wiki/New_Generation_Fighter">Voir plus</a>
+		        </div>
+		      </div>
+		    </div>
+		 </div>
+
+</section>
+
+</body>
+</html>
